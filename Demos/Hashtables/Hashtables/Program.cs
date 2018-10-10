e489de80d91dc22c0504de274fcb4d8d9fbc7c90
Altlock <altlockg@gmail.com>
2018-10-10 11:48:01 +0200

    Add matrix code to hashtable demo

--- Demos/Hashtables/Hashtables/Program.cs
+++ Demos/Hashtables/Hashtables/Program.cs
@@ -3,29 +3,39 @@ using System.Collections;
 
 namespace Hashtables
 {
-    partial class Program
+    partial class Constants
     {
-        private static void Main(string[] args)
-        {
-            Hashtable[,] game_board = Matrix.Make(Card.allUnique);
-            
-            Card.PrintAll();
+        public const int height = 4;
+        public const int width = 4;
 
-            Console.ReadKey();
-        }
-    }
-
-    partial class Card
-    {
         public static readonly Hashtable[] allUnique = new Hashtable[]
         {
             Card.New(1), Card.New(1), Card.New(2), Card.New(2), Card.New(3), Card.New(3),
             Card.New(4), Card.New(4), Card.New(5), Card.New(5), Card.New(6), Card.New(6),
             Card.New(7), Card.New(7), Card.New(8), Card.New(8)
         };
+    }
+
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Hashtable[,] game_board = Matrix.Make(Constants.allUnique);
+
+            //Card.PrintAll();
+            Matrix.PrintToConsole(game_board);
+            Console.ReadKey();
+        }
+    }
+
+    partial class Card
+    {
+        /// <summary>
+        /// Print all Cards in Constants.allUnique
+        /// </summary>
         public static void PrintAll()
         {
-            foreach (Hashtable table in Card.allUnique)
+            foreach (Hashtable table in Constants.allUnique)
             {
                 foreach (DictionaryEntry entry in table)
                 {
@@ -34,6 +44,11 @@ namespace Hashtables
             };
         }
 
+        /// <summary>
+        /// Make new card hashtable with all data needed
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
         public static Hashtable New(int number)
         {
             return new Hashtable
@@ -47,19 +62,60 @@ namespace Hashtables
 
     partial class Matrix
     {
+        /// <summary>
+        /// Make a new 2d hashtable array with correct height/width
+        /// </summary>
+        /// <returns>Empty matrix</returns>
         public static Hashtable[,] NewEmpty()
         {
-            return new Hashtable[,] { { }, { }, { }, { } };
+            return new Hashtable[Constants.height, Constants.width];
         }
 
+        /// <summary>
+        /// Fill array with all cards
+        /// </summary>
+        /// <param name="all cards"></param>
+        /// <returns>matrix filled with cards</returns>
         public static Hashtable[,] Make(Hashtable[] allCards)
         {
-            var empty_array = Matrix.NewEmpty(); // Make new matrix
-            // Shuffle allCards
-            // For each card in shuffled array
-            //
-            var matrix = Matrix.NewEmpty();
+            var matrix = Matrix.NewEmpty(); // Make new matrix
+            // TODO: Shuffle allCards
+            int counter = 0;
+
+            for (int i = 0; i < Constants.width; i++)
+            {
+                for (int j = 0; j < Constants.height; j++)
+                {
+                    //Logging
+                    //Console.WriteLine($"{i}, {j}");
+                    //Console.WriteLine($"{allCards[counter]["Number"]} \n");
+                    matrix[i, j] = allCards[counter];
+                    counter += 1;
+                }
+            }
             return matrix;
+        }
+
+        /// <summary>
+        /// Print matrix to console, in square
+        /// </summary>
+        /// <param name="matrix"></param>
+        public static void PrintToConsole(Hashtable[,] matrix)
+        {
+            for (int i = 0; i < Constants.width; i++)
+            {
+                for (int j = 0; j < Constants.height; j++)
+                {
+                    //Logging
+                    //Console.WriteLine($"{i}, {j}");
+
+                    Console.Write($"{matrix[i, j]["Number"]}");
+                    if (j == 3)
+                    {
+                        Console.Write("\n");
+                    }
+                }
+            }
         }
     }
 }
