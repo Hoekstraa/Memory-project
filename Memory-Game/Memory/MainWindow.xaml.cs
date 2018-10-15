380f006aba72a5a104a2e0879c3567ef941c2d70
Altlock <altlockg@gmail.com>
2018-10-15 16:28:24 +0200

    Here's the start

--- Memory-Game/Memory/MainWindow.xaml.cs
+++ Memory-Game/Memory/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,39 @@ namespace Memory
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Start of the program, makes calls to functions to:
+        /// make a new shuffled matrix * 6 and print them out 1 by 1.
+        /// </summary>
         public MainWindow()
         {
+            var gameBoard = Matrix.Make(Card.Shuffled(Constant.AllUnique));
+            Matrix.TraceBoard(gameBoard);
+
+            InitPlayScreen();
+            BoardToScreen(gameBoard);
+
             InitializeComponent();
         }
+
+
+        /// <summary>
+        /// Takes array and maps it onto the GUI
+        /// </summary>
+        /// <param name="matrix"></param>
+        private void BoardToScreen(Hashtable[,] matrix)
+        {
+            //
+        }
+
+        /// <summary>
+        /// Generates gameplay screen dynamically
+        /// </summary>
+        private void InitPlayScreen()
+        {
+            var cardGrid = new Grid {Name = "CardGrid"};
+        }
     }
+
+
 }
---
+++ Memory-Game/Memory/Matrix.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Memory
+{
+    /// <summary>
+    /// Holds code regarding matrices
+    /// </summary>
+    internal class Matrix
+    {
+        /// <summary>
+        /// Make a new 2d hashtable array with correct Height/Width
+        /// </summary>
+        /// <returns>Empty matrix</returns>
+        public static Hashtable[,] NewEmpty
+            => new Hashtable[Constant.Height, Constant.Width];
+
+        /// <summary>
+        /// Fill array with all cards
+        /// </summary>
+        /// <param name="allCards"></param>
+        /// <returns>matrix filled with cards</returns>
+        public static Hashtable[,] Make(Hashtable[] allCards)
+        {
+            var matrix = NewEmpty; // Make new matrix
+            // TODO: Shuffle allCards
+            var counter = 0;
+            for (var i = 0; i < Constant.Width; i++)
+            for (var j = 0; j < Constant.Height; j++)
+            {
+                //Logging
+                //Console.WriteLine($"{i}, {j}");
+                //Console.WriteLine($"{allCards[counter]["Number"]} \n");
+                matrix[i, j] = allCards[counter];
+                counter += 1;
+            }
+            return matrix;
+        }
+        /// <summary>
+        /// Print matrix to console, in a square
+        /// </summary>
+        /// <param name="matrix"></param>
+        public static void PrintToConsole(Hashtable[,] matrix)
+        {
+            for (var i = 0; i < Constant.Width; i++)
+            for (var j = 0; j < Constant.Height; j++)
+            {
+                Console.Write($"{matrix[i, j]["Number"]} ");
+                if (j == 3)
+                    Console.Write("\n");
+            }
+        }
+
+        public static void TraceBoard(Hashtable[,] matrix)
+        {
+            for (var i = 0; i < Constant.Width; i++)
+            for (var j = 0; j < Constant.Height; j++)
+            {
+                Trace.Write($"{matrix[i, j]["Number"]} ");
+                if (j == 3)
+                    Trace.WriteLine("");
+            }
+        }
+
+    }
+}
