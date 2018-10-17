a90763835b5ac465a5cb82b1bfaee3501d3968bd
Altlock <altlockg@gmail.com>
2018-10-17 10:49:47 +0200

    Refactor function to be 'pure'

--- Memory-Game/Memory/MainWindow.xaml.cs
+++ Memory-Game/Memory/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
@@ -31,47 +30,48 @@ namespace Memory
             mainWindow.Content = rootGrid;
             mainWindow.Show();
 
-            InitializeComponent();
+            //InitializeComponent();
         }
 
         /// <summary>
-        ///     Takes array and maps it onto the GUI
+        ///     Takes card matrix maps its images onto the GUI
         /// </summary>
-        /// <param name="matrix"></param>
-        /// <param name="cardGrid"></param>
+        /// <param name="matrix">Representation of gameboard in 2d array of hashtables</param>
+        /// <param name="cardGrid">Grid to be filled with Images</param>
+        /// <returns>New modified grid</returns>
         private Grid FillCardGrid(Hashtable[,] matrix, Grid cardGrid)
         {
+            var newGrid = cardGrid;
             for (var i = 0; i < 4; i++)
             for (var j = 0; j < 4; j++)
             {
                 var y = matrix[i, j]["Number"];
-                Trace.WriteLine(y.GetType());
                 var x = CreateImage(i + 1);
-                cardGrid.Children.Add(x);
+                newGrid.Children.Add(x);
                 Grid.SetRow(x, j);
                 Grid.SetColumn(x, i);
             }
 
-            return cardGrid;
+            return newGrid;
         }
 
         /// <summary>
-        ///     Add x amount of columns to a grid
+        ///     Add x amount of columns to a grid, MUTATES the grid
         /// </summary>
-        /// <param name="grid"></param>
-        /// <param name="amount"></param>
-        private void AddColumns(Grid grid, int amount)
+        /// <param name="grid">Grid to add columns to</param>
+        /// <param name="amount">Amount of columns to be added</param>
+        private void AddColumns(Grid grid, int amount = 1)
         {
             for (var i = 0; i < amount; i++)
                 grid.ColumnDefinitions.Add(new ColumnDefinition());
         }
 
         /// <summary>
-        ///     Add x amount of rows to a grid
+        ///     Add x amount of rows to a grid, MUTATES the grid
         /// </summary>
-        /// <param name="grid"></param>
-        /// <param name="amount"></param>
-        private void AddRows(Grid grid, int amount)
+        /// <param name="grid">Grid to add rows to</param>
+        /// <param name="amount">Amount of rows to be added</param>
+        private void AddRows(Grid grid, int amount = 1)
         {
             for (var i = 0; i < amount; i++)
                 grid.RowDefinitions.Add(new RowDefinition());
@@ -80,6 +80,7 @@ namespace Memory
         /// <summary>
         ///     Generates gameplay screen dynamically
         /// </summary>
+        /// <returns>New grid</returns>
         private Grid GenerateCardGrid()
         {
             var cardGrid = new Grid {Name = "CardGrid", ShowGridLines = true};
@@ -94,7 +95,7 @@ namespace Memory
         ///     Returns Image based on the cardNumber
         /// </summary>
         /// <param name="number"></param>
-        /// <returns></returns>
+        /// <returns>new image</returns>
         private Image CreateImage(int number)
         {
             var simpleImage = new Image {Width = 200, Margin = new Thickness(5)};
